4233464efdcd410888831055b8440a75715ee13c
bliona <lionamuhati16@gmail.com>
2023-05-11 11:01:43 -0700

    Python

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def magic_calculation(x, y):
+    """Does samething as bytecode provided in project."""
+    from magic_calculation_102 import add, sub
+
+    if x < y:
+        z = add(x, y)
+        for i in range(4, 6):
+            z = add(z, i)
+        return (z)
+
+    else:
+        return(sub(x, y))
